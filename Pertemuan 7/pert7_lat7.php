9dea2f59013bd138ce66d4d92d31a52f6c973a3a
ajidwiyatna <80055514+ajidwiyatna@users.noreply.github.com>
2021-04-08 22:09:24 +0700

    Add files via upload

---
+++ Pertemuan 7/pert7_lat7.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Penggunaan Split</title>
+</head>
+<body>
+
+	<?php
+		$tanggal = "17-05-2010";
+		list($hari, $bulan, $tahun) = explode("-", $tanggal);
+		echo "Hari = $hari";
+		echo "<br />";
+		echo "Bulan = $bulan";
+		echo "<br />";
+		echo "Tahun = $tahun";
+	?>
+
+</body>
+</html>
