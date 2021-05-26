b071e5270c0f57aa278c1279f49357c377344369
ajidwiyatna <80055514+ajidwiyatna@users.noreply.github.com>
2021-05-26 21:39:12 +0700

    Create form.php

---
+++ Pertemuan 11/form.php
@@ -0,0 +1,12 @@
+<html> 
+<body>
+	<h3>Nama : Aji Dwiyatna</h3>
+	<h3>NIM : 171011401048</h3>
+	<form action="insert3.php" method="post"> 
+		Firstname: <input type="text" name="firstname"/><br> 
+		Lastname: <input type="text" name="lastname"/><br> 
+		Age: <input type="text" name="age"/><br>
+		<input type="submit"/>
+	</form> 
+</body> 
+</html> 
